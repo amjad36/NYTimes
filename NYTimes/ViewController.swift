1c305f95fc75560418958a59e96987615422e48c
Amjad Khan <khan.amjad36@gmail.com>
2018-10-20 00:16:47 +0530

    Initial Commit

---
+++ NYTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTimes
+//
+//  Created by Amjad Khan on 20/10/18.
+//  Copyright © 2018 Amjad Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
